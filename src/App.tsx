b7c033548be00c09f7cb705682035b4458bbc9b8
yeon <ohy9205@naver.com>
2024-08-02 21:37:45 +0900

    계산결과 영역 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="w-[400px] h-[367px] bg-[#242530] p-[25px]">
+      <div className="bg-[#3A3F77] h-[70px] pt-[18px] pr-[16px] text-right rounded-[20px]">
+        <span className="text-[32px] leading-[38.73px]">0 &nbsp;</span>
+      </div>
+    </div>
+  );
 }
 
 export default App;
